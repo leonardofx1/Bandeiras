cfe86a05785ea21bd7971b8fdef09e499950a81d
leonardofx1 <leonardonunesfreitas1@gmail.com>
2023-02-06 12:18:09 -0300

    'dark-mode'

--- assets/js/darkMode.js
+++ assets/js/darkMode.js
@@ -1,16 +1,20 @@
 const icon = document.querySelector('[data-js="iconThema"]')
 const btn = document.querySelector('.btn__mode')
 const body = document.querySelector('[data-js="body"]')
-const card = document.querySelector('.card__container')
+
 const cabecalho = document.querySelector('.cabecalho')
 const inputTxt = document.querySelector('.input__txt')
 const inputSelect = document.querySelector('.input__select')
 
+const cardDark = async () =>{
+    const card = document.querySelectorAll('.card__container')
+    const cardDark = [...card].forEach(card => card.classList.toggle('card__dark'))
+}
+
 btn.addEventListener('click', e => {
     body.classList.toggle('body__dark')
-    card.classList.toggle('card__dark')
     cabecalho.classList.toggle('cabecalho__dark')
+    cardDark()
     inputSelect.classList.toggle('input__dark')
     inputTxt.classList.toggle('input__dark')
-    console.log('oi')
 })
