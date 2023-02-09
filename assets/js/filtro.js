12290aa7d7b109c6d3fdfe61db7ecaddb402d255
leonardofx1 <leonardonunesfreitas1@gmail.com>
2023-02-09 11:50:46 -0300

    'filtro select'

--- assets/js/filtro.js
+++ assets/js/filtro.js
@@ -1,9 +1,22 @@
 const inputTXT = document.querySelector('.input__txt')
-inputTXT.addEventListener('input', e => {
-    const card = document.querySelectorAll('.card__container')
-    const cardDark = [...card].filter(card => !card.textContent.toUpperCase().includes(e.target.value.toUpperCase()))
+const select = document.querySelector('.input__select')
+
+const filtro = ([...bandeiras], pesquisa) => {
+    bandeiras.filter((bandeira => !bandeira.textContent.toUpperCase().includes(pesquisa.toUpperCase())))
     .forEach(card => card.style.display ='none')
-    const car = [...card].filter(card => card.textContent.toUpperCase().includes(e.target.value.toUpperCase()))
+    bandeiras.filter((bandeira => bandeira.textContent.toUpperCase().includes(pesquisa.toUpperCase() ,console.log(bandeira))))
     .forEach(card => card.style.display ='block')
+}
+
+
+select.addEventListener('change', ({target})=> {
+    const card = document.querySelectorAll('.card__container')
+    filtro(card,target.value)
+})
+
+
+inputTXT.addEventListener('input', ({target}) => {
+    const card = document.querySelectorAll('.card__container')
+    filtro(card, target.value)
 })
 
